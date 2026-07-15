cfd769f1bdb3f1b90c3524c0406a1ff52e665a89
Gian Paolo Mulingbayan <mulingbayan.gianpaolo@gmail.com>
2026-07-15 15:06:25 +0800

    fix web icon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -32,7 +32,7 @@
         </style>
 
         <link rel="icon" href="/favicon.svg" type="image/svg+xml">
-        <link rel="icon" href="/favicon.ico" sizes="any">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
 
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=inter:400,500,600,700" rel="stylesheet" />
